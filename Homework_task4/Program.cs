0517affb335cdc85809a3d09556bbb6800d0c396
Viktoriia <rizaeva.vika@yandex.ru>
2022-06-07 14:34:28 +0300

    Homework task 4

---
+++ Homework_task4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Clear();
+
+Console.Write("Введите натуральное число:");
+
+string input = Console.ReadLine(); 
+int number = Convert.ToInt32(input);
+
+int i = 1;
+
+if (number > 0)
+{
+while (i <= number)
+{
+    if ( i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+
+}
+}
+else Console.WriteLine("ОШИБКА! Введено не натуральное число");
