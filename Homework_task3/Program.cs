d2e817dd9e8d993dea6a4d8f39c5eff17e66cc1d
Viktoriia <rizaeva.vika@yandex.ru>
2022-06-07 13:17:29 +0300

    Homework task3

---
+++ Homework_task3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.Write("Введите число:");
+
+string input = Console.ReadLine(); 
+double number = Convert.ToInt32(input);
+
+if (number % 2 == 0)
+{
+  Console.WriteLine("Введенное число " + number + " четное");
+}
+
+else 
+{
+ Console.WriteLine("Введенное число " + number + " нечетное");
+}
