1d3fabc50462f15b5d384a5ff4619a7cc374846b
Viktoriia <rizaeva.vika@yandex.ru>
2022-06-14 16:27:32 +0300

    Homework Seminar 3

---
+++ Homework_Seminar3/S3_task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Clear();
+
+int number = Input("Введите число: ");
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+if ((number > 9999) && (number < 100000))
+{
+    int firstNum = number / 10000;
+    int lastNum = number % 10;
+    int secNum = (number / 1000) % 10;
+    int preLastnum = (number % 100) / 10;
+    if ((firstNum == lastNum) && (secNum == preLastnum))
+    Console.WriteLine("Да - палиндром");
+    else  Console.WriteLine("Нет - не палиндром");
+}
+else Console.WriteLine("Ошибка! Введено не пятизначное число");
+
---
+++ Homework_Seminar3/S3_task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+int number = Input("Введите число: ");
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int i = 1;
+
+while (i <= number)
+{
+    if (i<number) Console.Write(i*i*i + ", ");
+    else Console.Write(i*i*i);
+    i++;
+}
