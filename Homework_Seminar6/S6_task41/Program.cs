84bdddbf9c47a3e501c2c7d0a7236ebcc1f9a72d
Viktoriia <rizaeva.vika@yandex.ru>
2022-06-27 14:32:31 +0300

    Homework Seminar 6

---
+++ Homework_Seminar6/S6_task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.Write("Введите элементы(через зяпятую): ");
+string input = Console.ReadLine();
+
+int countAll = 1;
+int countNegative = 0;
+int countPositive = 0;
+int i = 0;
+int length = input.Length;
+
+if (input[0] == 44)
+{
+    i++;
+}
+
+if (input[length - 1] == 44)
+{
+    length--;
+}
+
+for (; i < length; i++)
+{
+    char item = input[i];
+    if (item == 44) // ,
+    {
+        countAll++;
+        continue;
+    }
+
+    if (item == 45) // -
+    {
+        countNegative++;
+        continue;
+    }
+
+    if (item == 48) // 0
+    {
+        countNegative++;
+    }
+}
+
+countPositive = countAll - countNegative;
+Console.Write($"Кол-во элементов > 0: {countPositive}");
+
+
---
+++ Homework_Seminar6/S6_task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+Console.Clear();
+
+double b1 = Input("Введите b1: ");
+double k1 = Input("Введите k1: ");
+double b2 = Input("Введите b2: ");
+double k2 = Input("Введите k2: ");
+
+double Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double x = (b2 - b1) / (k1-k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"x=: {x}");
+Console.WriteLine($"y=: {y}");
